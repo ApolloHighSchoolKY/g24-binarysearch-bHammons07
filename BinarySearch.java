bb6319e8b4da99c7c99a67527386fba587e6f1a5
bHammons07 <brody.hammons@stu.daviess.kyschools.us>
2025-04-16 14:08:46 +0000

    FIRST TRY

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,26 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
-		
+		Arrays.sort(ray);
+		int bottom = 0 ;
+		int top = ray.length-1;
+		int secret = item;
+		int mid = 0;
+
+		while(bottom <= top)
+		{
+			mid = (bottom+top)/2;
+
+			if(ray[mid] < secret)
+
+				bottom = mid + 1;
+
+			else if(ray[mid] > secret)
+
+				top = mid - 1;
+			else
+				return mid;
+		}
+		return -1;
 	}
 }
